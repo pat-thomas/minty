0ef7d90f08b49ffa4ca39ee320eb2a8fb16514f0
Pat Thomas <pthomas@relaynetwork.com>
2015-04-06 08:59:12 -0400

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to minty
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/minty/core_test.clj
@@ -0,0 +1,7 @@
+(ns minty.core-test
+  (:require [clojure.test :refer :all]
+            [minty.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
