bae3d803944101749be7eedc829f1cedd800799e
Pat Thomas <patthomassoftware@gmail.com>
2015-04-07 21:16:54 -0400

    Basic tests for compiler.

--- test/minty/core_test.clj
+++ test/minty/core_test.clj
@@ -1,7 +1,24 @@
 (ns minty.core-test
-  (:require [clojure.test :refer :all]
-            [minty.core :refer :all]))
+  (:require [minty.core   :as core]
+            [clojure.test :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest no-type-annotations
+  (testing "it works"
+    (is (= (core/parse-body '([a b]
+                              (+ a b)))
+           {:args-vec    '[a b]
+            :return-type false
+            :typed-defn? false}))))
+
+(deftest type-annotations
+  (testing "it works"
+    (is (= (core/parse-body '([a :- Int b :- Int]
+                              (+ a b)))
+           {:args-vec    '[a :- Int b :- Int]
+            :return-type false
+            :typed-defn? true}))
+    (is (= (core/parse-body '([a :- Int b :- Int] :- Int
+                              (+ a b)))
+           {:args-vec    '[a :- Int b :- Int]
+            :return-type 'Int
+            :typed-defn? true}))))
