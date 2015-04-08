3cc308a6ca218b012013b05c19c2f468b80c56e9
Pat Thomas <patthomassoftware@gmail.com>
2015-04-07 21:39:29 -0400

    Clean up, remove custom typechecking because it doesn't work.

--- src/minty/core.clj
+++ src/minty/core.clj
@@ -11,44 +11,31 @@
 
 (t/defn ^:no-check parse-body
   [body]
-  (let [args-vec    (and (vector? (first body))
+  (let [doc-string  (and (string? (first body))
                          (first body))
-        return-type (and (> (count body) 2)
-                         (= (nth body 1) :-)
-                         (nth body 2))
+        args-vec    (if doc-string
+                      (and (vector? (second body))
+                           (second body))
+                      (and (vector? (first body))
+                           (first body)))
+        return-type (if doc-string
+                      (and (> (count body) 3)
+                           (= (nth body 2) :-)
+                           (nth body 3))
+                      (and (> (count body) 2)
+                           (= (nth body 1) :-)
+                           (nth body 2)))
         typed-defn? (typed-args-vec? args-vec)]
     (if-not args-vec
       :error
       {:args-vec    args-vec
+       :doc-string  doc-string
        :return-type return-type
        :typed-defn? typed-defn?})))
-
-(t/defn ^:no-check try-check-ns
-  []
-  (try
-    (t/check-ns)
-    (catch Exception ex
-      ex)))
-
-(defmacro type-check
-  ([]
-   `(type-check ~*ns*))
-  ([target-ns]
-   (let [unannotated       (->> target-ns
-                                ns-publics
-                                vals
-                                (map meta)
-                                (filter #(= (:no-check %) true))
-                                (map :name)
-                                sort)
-         type-check-result (try-check-ns)]
-     (if-not (= type-check-result :ok)
-       (throw type-check-result)
-       {:unannotated unannotated}))))
 
 (defmacro defn
   [fn-name & body]
   (let [{:keys [typed-defn? args-vec return-type fn-body] :as parse-result} (parse-body body)]
     (if typed-defn?
       `(t/defn ~fn-name ~@body)
-      `(t/defn ~(with-meta fn-name {:no-check true}) ~fn-name ~@body))))
+      `(t/defn ~(with-meta fn-name {:no-check true}) ~@body))))
